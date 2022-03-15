724f2090bbda2d47fde919ebb9f160bc93f44ca7
Zartab Nakhwa <zartab@codewithz.com>
2022-03-15 14:43:04 +0530

    Redux: Dispatch the Actions

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import store from "./store";
+
+store.dispatch({
+    type: 'ADD_BUG',
+    payload: {
+        description: 'Bug 1'
+    }
+});
+
+// console.log(store)
+console.log(store.getState())
+
+store.dispatch({
+    type: 'REMOVE_BUG',
+    payload: {
+        id: 6
+    }
+})
+console.log(store.getState())
