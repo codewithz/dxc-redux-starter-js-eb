f449bd7fa397231d61c09698a98127903ede1296
Zartab Nakhwa <zartab@codewithz.com>
2022-03-15 15:10:38 +0530

    Redux: Dev Tools Data Added

--- src/store.js
+++ src/store.js
@@ -2,7 +2,8 @@ import { createStore, combineReducers } from 'redux';
 import { reducer } from './reducer';
 
 
-const store = createStore(reducer);
+const store = createStore(reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
 
